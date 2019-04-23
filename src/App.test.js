30080b0dcb12fd65cc4033d9568687f92bf745d3
Olabode Adeloye <bodeadeloye@gmail.com>
2019-04-23 15:03:50 +0100

    Testing completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import NavBar from "./components/Layout/NavBar/NavBar";
+import Footer from "./components/Layout/Footer/Footer";
+//import SignInWidget from "./components/auth/SignInWidget";
+import Home from "./components/pages/Home/Home";
+import GeneralTraining from "./components/Training/GeneralTraining";
+import UserRequestsPage from "./components/pages/UserRequests/UserRequestPage";
+//import Login from "./components/auth/Login";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("NavBar", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<NavBar debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("Footer", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<Footer debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+//Does not work when attempting to deploy application
+// describe("Login", () => {
+//   it('should render correctly in "debug" mode', () => {
+//     const component = shallow(<Login debug />);
+
+//     expect(component).toMatchSnapshot();
+//   });
+// });
+
+//Does not work when attempting to deploy application
+// describe("SignInWidget", () => {
+//   it('should render correctly in "debug" mode', () => {
+//     const component = shallow(<SignInWidget debug />);
+
+//     expect(component).toMatchSnapshot();
+//   });
+// });
+
+describe("Home", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<Home debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("GeneralTraining", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<GeneralTraining debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("UserRequestsPage", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<UserRequestsPage debug />);
+
+    expect(component).toMatchSnapshot();
+  });
 });
